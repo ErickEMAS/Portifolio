1ad04603b9f09da1a23dc7719a8c52218565ead6
erickemas <erick8alexandre@gmail.com>
2020-05-12 18:07:29 -0300

    Adiciona menu lateral na versão mobile

--- js/scripts.js
+++ js/scripts.js
@@ -9,3 +9,15 @@ $(".nav-link").click(function(e){
         scrollTop: targetoffset - 0
     }, 500)
 });
+
+//menu mobile
+$(document).ready(function(){
+
+    $(".open-nav").click(function(){
+        let altura = window.innerHeight;
+        document.querySelector("header").style.height = `${altura}px`;
+        $("header").toggleClass("active");
+        $(".open-nav").toggleClass("close");
+    });
+    
+});
